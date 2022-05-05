5614ae74c64e5259dce16b97c4a5f1f01f1df7d6
Dissmind <dissmind@gmail.com>
2022-05-05 02:03:33 +0200

    [ADD]: ts included

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import * as ReactDOM from 'react-dom'
+
+const root = (
+    <React.StrictMode>
+        <h1>React hello</h1>
+    </React.StrictMode>
+)
+
+
+ReactDOM.render(root, document.querySelector('#root'))
